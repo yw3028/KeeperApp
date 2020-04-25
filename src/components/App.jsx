49730a09ebf2434d49a2081f7065172adad8006b
Yu <berylwangyu@gmail.com>
2020-04-25 12:35:50 +0200

    map notes.js

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,15 +2,19 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from "../notes";
 
-function App(){
-    return (<div>
-        <Header />
-        <Note />
-        <Footer />
-    </div>)
-};
+function App() {
+  return (
+    <div>
+      <Header />
+      {notes.map((note) => (
+        <Note key={note.id} title={note.title} content={note.content} />
+      ))}
 
-
+      <Footer />
+    </div>
+  );
+}
 
 export default App;
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import notes from "../notes";
 
-function Note(){
+function Note(props){
     return <div className="note">
-        <h1>This is the note title</h1>
-        <p>THis is the note content</p>
+        <h1>{props.title}</h1>
+        <p>{props.content}</p>
     </div>
 };
 
